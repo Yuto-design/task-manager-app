d040135d04337a36d3b67fc12d2d7be08340d1db
Y.S <c1320527@st.kanazawa-it.ac.jp>
2026-01-22 14:31:58 +0900

    Update App

--- routes/web.php
+++ routes/web.php
@@ -7,6 +7,13 @@ Route::get('/', function () {
     return redirect('/login');
 });
 
-Route::get('/tasks', [TaskController::class, 'index']);
+Route::get('/tasks', [TaskController::class, 'index'])
+    ->name('tasks.index');
+
+
+Route::middleware('auth')->group(function () {
+    Route::get('/tasks', [TaskController::class, 'index'])
+        ->name('tasks.index');
+});
 
 require __DIR__.'/auth.php';
